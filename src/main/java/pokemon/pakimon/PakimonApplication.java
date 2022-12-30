b2018f225f8764a9dc4e288656e7f02980f6ee1e
Jorge Boggen <jboggenmontanares@gmail.com>
2022-12-30 19:37:53 -0300

    Base Fallida
    Base con problemas en configuracion MySQL

---
+++ src/main/java/pokemon/pakimon/PakimonApplication.java
@@ -0,0 +1,13 @@
+package pokemon.pakimon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PakimonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pokemon/pakimon/PakimonApplicationTests.java
@@ -0,0 +1,13 @@
+package pokemon.pakimon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
